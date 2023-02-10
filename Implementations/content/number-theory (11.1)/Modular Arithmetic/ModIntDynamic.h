652a4c620b5b4a2872b049574891e0c536822a11
David Clyde <dcclyde@gmail.com>
2023-02-10 02:04:17 -0800

    bugfixes in ModIntDynamic

--- Implementations/content/number-theory (11.1)/Modular Arithmetic/ModIntDynamic.h
+++ Implementations/content/number-theory (11.1)/Modular Arithmetic/ModIntDynamic.h
@@ -1,49 +1,53 @@
+#pragma region  // mint (dynamic)
 /**
- * Description: modular arithmetic operations 
- * Source: 
+ * Description: modular arithmetic operations
+ * Source:
 	* KACTL
 	* https://codeforces.com/blog/entry/63903
 	* https://codeforces.com/contest/1261/submission/65632855 (tourist)
 	* https://codeforces.com/contest/1264/submission/66344993 (ksun)
 	* also see https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp (ecnerwal)
 	* https://github.com/atcoder/ac-library/blob/master/atcoder/modint.hpp
- * Verification: 
+ * Verification:
 	* https://open.kattis.com/problems/modulararithmetic
  */
 
 template<int id> struct mint {
 	static FastMod MF;
 	static int mod() { return (int)MF.b; }
-	static void set_mod(int m) { assert(1 <= m); MF = FastMod(m); }
+	static int mod_totient;
+	static int modphi() { return mod_totient; }
+	static void set_mod(int p) { assert(1 <= p); MF = FastMod(p); mod_totient = p-1;}
+	static void set_mod(int m, int phi) { assert(1 <= m); MF = FastMod(m); mod_totient = phi;}
 	int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
 	mint() { v = 0; }
 	mint(ll _v) { v = int((-mod() < _v && _v < mod()) ? _v : _v % mod());
 		if (v < 0) v += mod(); }
-	friend bool operator==(const mint& a, const mint& b) { 
+	friend bool operator==(const mint& a, const mint& b) {
 		return a.v == b.v; }
-	friend bool operator!=(const mint& a, const mint& b) { 
+	friend bool operator!=(const mint& a, const mint& b) {
 		return !(a == b); }
-	friend bool operator<(const mint& a, const mint& b) { 
+	friend bool operator<(const mint& a, const mint& b) {
 		return a.v < b.v; }
 	friend void re(mint& a) { ll x; re(x); a = mint(x); }
 	friend str ts(mint a) { return ts(a.v); }
-   
-	mint& operator+=(const mint& m) { 
-		if ((v += m.v) >= mod()) v -= mod(); 
+
+	mint& operator+=(const mint& m) {
+		if ((v += m.v) >= mod()) v -= mod();
 		return *this; }
-	mint& operator-=(const mint& m) { 
-		if ((v -= m.v) < 0) v += mod(); 
+	mint& operator-=(const mint& m) {
+		if ((v -= m.v) < 0) v += mod();
 		return *this; }
-	mint& operator*=(const mint& m) { 
+	mint& operator*=(const mint& m) {
 		v = (int)MF.reduce((ll)v*m.v); return *this; }
 	mint& operator/=(const mint& m) { return (*this) *= inv(m); }
 	friend mint pow(mint a, ll p) {
 		mint ans = 1; assert(p >= 0);
 		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
 		return ans; }
-	friend mint inv(const mint& a) { assert(a.v != 0); 
-		return pow(a,mod()-2); }
-		
+	friend mint inv(const mint& a) { assert(a.v != 0); assert(mod_totient != -1);
+		return pow(a,modphi()-1); }
+
 	mint operator-() const { return mint(-v); }
 	mint& operator++() { return *this += 1; }
 	mint& operator--() { return *this -= 1; }
@@ -53,6 +57,7 @@ template<int id> struct mint {
 	friend mint operator/(mint a, const mint& b) { return a /= b; }
 };
 template<int id> FastMod mint<id>::MF = FastMod(1);
+template<int id> int mint<id>::mod_totient = -1;
 
 typedef mint<0> mi; // 5 is primitive root for both common mods
 typedef vector<mi> vmi;
@@ -62,6 +67,12 @@ typedef vector<pmi> vpmi;
 vector<vmi> scmb; // small combinations
 void genComb(int SZ) {
 	scmb.assign(SZ,vmi(SZ)); scmb[0][0] = 1;
-	FOR(i,1,SZ) F0R(j,i+1) 
+	FOR(i,1,SZ) F0R(j,i+1)
 		scmb[i][j] = scmb[i-1][j]+(j?scmb[i-1][j-1]:0);
 }
+
+template <int id>
+string to_string(mint<id> modint) {
+    return to_string((int)modint);
+}
+#pragma endregion
